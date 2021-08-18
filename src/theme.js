8acd6d3be5bf476aa261d3114d5569780028bce5
Marcelo <marcelo.nasccc@gmail.com>
2021-08-17 21:49:54 -0300

    feat: create theme file

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+/**
+ * cinza 1 = #C6CDCE
+ */
+
+export default {
+  accent: '#000000',
+  danger: '#E66060',
+  blue: '#39C6BB',
+  yellow: '#F2C94C',
+  darkText: '#1E1F1C',
+  grayText: '#626F72',
+  background: '#F5F7F8',
+  lightText: '#FFFFFF',
+};
