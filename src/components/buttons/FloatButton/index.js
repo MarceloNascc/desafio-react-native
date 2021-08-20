6386277f2bebbe0592858335a9793ea1f55e8a90
Marcelo <marcelo.nasccc@gmail.com>
2021-08-19 22:56:53 -0300

    feat: add disabled prop

--- src/components/buttons/FloatButton/index.js
+++ src/components/buttons/FloatButton/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import {ButtonContainer} from './styles';
 
-const FloatButton = ({action, children, extraContainerStyle}) => {
+const FloatButton = ({disabled, action, children, extraContainerStyle}) => {
   return (
-    <ButtonContainer style={extraContainerStyle} onPress={action}>
+    <ButtonContainer
+      disabled={disabled}
+      style={extraContainerStyle}
+      onPress={action}>
       {children}
     </ButtonContainer>
   );
