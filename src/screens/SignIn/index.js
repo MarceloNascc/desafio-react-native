2ef6d2e427e3816c1ddebae5aa6ac169bb2d5b3f
Marcelo <marcelo.nasccc@gmail.com>
2021-08-19 22:31:12 -0300

    feat: integrate SignIn screen

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 import Button from '../../components/buttons/Button';
 import Input from '../../components/Input';
@@ -7,10 +8,37 @@ import {View, BackHandler} from 'react-native';
 import {Text, Icon} from './styles';
 import {ScreenContainer, Title} from '../../global.style';
 
+import {signIn} from '../../store/actions/auth';
+
 const SignIn = () => {
+  const dispatch = useDispatch();
+  const {loading} = useSelector(state => state.auth);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [seePassword, setSeePassword] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  function isValid() {
+    const hasErrors = {};
+
+    if (!email) {
+      hasErrors.email = true;
+    }
+
+    if (!password) {
+      hasErrors.password = true;
+    }
+
+    setErrors(hasErrors);
+
+    return password && email;
+  }
+
+  function handlerSignIn() {
+    if (isValid()) {
+      dispatch(signIn(email, password));
+    }
+  }
 
   return (
     // eslint-disable-next-line react-native/no-inline-styles
@@ -20,9 +48,15 @@ const SignIn = () => {
       <Title>Acesse sua conta</Title>
 
       <View>
-        <Input label="EMAIL" value={email} setValue={setEmail} />
+        <Input
+          error={errors.email}
+          label="EMAIL"
+          value={email}
+          setValue={setEmail}
+        />
 
         <Input
+          error={errors.password}
           label="SENHA"
           value={password}
           setValue={setPassword}
@@ -37,7 +71,7 @@ const SignIn = () => {
         />
       </View>
 
-      <Button action={() => console.log('sign in')} text="Entrar" />
+      <Button loading={loading} action={handlerSignIn} text="Entrar" />
     </ScreenContainer>
   );
 };
